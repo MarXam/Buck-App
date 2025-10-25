ad6271d4a5ff7ca74142148b53a76c84a36359ce
MarXam <kevinsantiago.arangoperafan@gmail.com>
2025-10-24 20:06:10 -0500

    refactor:se hacen cambios al PerfilActivity.java

--- app/src/main/java/com/marxam/buck/PerfilActivity.java
+++ app/src/main/java/com/marxam/buck/PerfilActivity.java
@@ -1,7 +1,5 @@
 package com.marxam.buck;
 
-import android.os.Bundle;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -17,53 +15,48 @@ public class PerfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
+
         String usuario = getIntent().getStringExtra("usuario");
+
         TextView tvUsuario = findViewById(R.id.usuario);
+
         if (usuario != null) {
             tvUsuario.setText("Usuario: " + usuario);
         } else {
             tvUsuario.setText("Usuario: Invitado");
-            Button btnSeguidos = findViewById(R.id.seguidos);
-            Button btnHistorial = findViewById(R.id.historial);
-            Button btnAjustes = findViewById(R.id.ajustes);
-            Button btnCerrarSesion = findViewById(R.id.cerrarseccion);
-
-            btnSeguidos.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent iSeguidos = new Intent(PerfilActivity.this, seguidosactivity.class);
-                    startActivity(iSeguidos);
-                }
-            });
-
-            btnHistorial.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent iHistorial = new Intent(PerfilActivity.this, historialactivity.class);
-                    startActivity(iHistorial);
-                }
-            });
-
-            btnAjustes.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent iAjustes = new Intent(PerfilActivity.this, ajustesactivity.class);
-                    startActivity(iAjustes);
-                }
-            });
-            btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(PerfilActivity.this, "Sesión cerrada", Toast.LENGTH_SHORT).show();
-                    Intent iLoginActivity = new Intent(PerfilActivity.this, LoginActivity.class);
-                    iLoginActivity.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(iLoginActivity);
-                    finish();
-                }
-            });
-
-            BarraNavegacionHelper.setup(this, R.id.bmPerfil);
-
         }
-    }}
 
+
+        Button btnSeguidos = findViewById(R.id.seguidos);
+        Button btnHistorial = findViewById(R.id.historial);
+        Button btnAjustes = findViewById(R.id.ajustes);
+        Button btnCerrarSesion = findViewById(R.id.cerrarseccion);
+
+        btnSeguidos.setOnClickListener(v -> {
+            Intent iseguidos = new Intent(PerfilActivity.this, seguidosactivity.class);
+            startActivity(iseguidos);
+        });
+
+
+        btnHistorial.setOnClickListener(v -> {
+            Intent ihistorial = new Intent(PerfilActivity.this, historialactivity.class);
+            startActivity(ihistorial);
+        });
+
+        btnAjustes.setOnClickListener(v -> {
+            Intent iajustes = new Intent(PerfilActivity.this, ajustesactivity.class);
+            startActivity(iajustes);
+        });
+
+        btnCerrarSesion.setOnClickListener(v -> {
+            Toast.makeText(PerfilActivity.this, "Sesión cerrada", Toast.LENGTH_SHORT).show();
+            Intent iloginactivity = new Intent(PerfilActivity.this, LoginActivity.class);
+            iloginactivity.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(iloginactivity;
+            finish();
+        });
+
+
+        BarraNavegacionHelper.setup(this, R.id.bmPerfil);
+    }
+}
